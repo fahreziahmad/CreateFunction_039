5da73f4d7340a14afa057af10ebc25aa04c206d8
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-10-21 11:04:36 +0700

    #fungsi lambda untuk menghitung lngkaran

---
+++ pertemuan4.py
@@ -0,0 +1,11 @@
+import math
+
+#fungsi lambda untuk menghitung lngkaran
+luas_lingkaran = lambda r: math.pi * r**2 #menghitung luas lingkaran
+
+#input dari pengguna
+jari_jari = float(input("Masukkan jari-jari Lingkaran: "))
+
+#menghitung dan mencetak hasil luas lingkaran
+hasil_luas =luas_lingkaran(jari_jari)
+print(f"Luas Lingkaran dengan jari-jari {jari_jari} adalah: {hasil_luas:.2f}")
